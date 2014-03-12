5b6e2e29b7c9714205b915c88b9e5990e21e6288
andrewleo <hz_liuxiao@corp.netease.com>
2014-03-12 13:47:30 +0800

    Added: iframe for IE

--- src/com/netease/test/TypeOperations.java
+++ src/com/netease/test/TypeOperations.java
@@ -17,7 +17,7 @@ public class TypeOperations {
 
 	BrowserEmulator be;
 	String input = "hello wrold !";
-	String inputForIE = "hello\" \"world\" \"!";
+	String inputForIE = "hello";
 
 	@BeforeClass
 	public void doBeforeTest() {
@@ -44,7 +44,7 @@ public class TypeOperations {
 			be.inputKeyboard(inputForIE);
 			driver.switchTo().defaultContent();
 			be.click("//input[@value='提交<iframe>节点文本']");
-			be.expectElementExistOrNot(true, "//h1[contains(text(),'" + input + "')]", 5000);
+			be.expectElementExistOrNot(true, "//h1[contains(text(),'" + inputForIE + "')]", 5000);
 			be.open("http://" + CommonFunction.ip + ":" + CommonFunction.port + "/type");
 		} else {
 			CommonFunction.typeInIframe(be, input);
