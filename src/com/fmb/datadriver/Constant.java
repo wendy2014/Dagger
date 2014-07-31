cc0e39aec09b169e6774d0259b6eed04e3f7ce33
Wendy <Shuwen.Zhang@linktone.com>
2014-07-31 18:41:56 +0800

    modified the Constant.java file and change the path

--- src/com/fmb/datadriver/Constant.java
+++ src/com/fmb/datadriver/Constant.java
@@ -2,7 +2,7 @@ package com.fmb.datadriver;
 
 public class Constant 
 {
-	public static final String Path_TestData = "G://git//Dagger//data//";
+	public static final String Path_TestData = ".//data//";
 	public static final String File_TestData = "TestData.xlsx";
 	public static final String Sheet = "Login";
 
